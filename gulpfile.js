980ac1442f4e0e1b27159ee9e7a5487ee0789917
KingPra <kingi.pra@gmail.com>
2017-05-16 22:50:14 -0400

    changed link on html

--- gulpfile.js
+++ gulpfile.js
@@ -9,38 +9,38 @@ gulp.task('default', ['html', 'css', 'js', 'images', 'fonts']);
 gulp.task('html', () => {
   return gulp.src('index.html')
   .pipe(strip())
-  .pipe(gulp.dest('docs/'))
+  .pipe(gulp.dest('public_html/'))
 });
 
 gulp.task('css', () => {
   return gulp.src('style.scss')
   .pipe(sass())
   .pipe(strip.text())
-  .pipe(gulp.dest('docs/css'))
+  .pipe(gulp.dest('public_html/css'))
 });
 
 gulp.task('css', () => {
   return gulp.src('css/*')
   .pipe(strip.text())
-  .pipe(gulp.dest('docs/css'))
+  .pipe(gulp.dest('public_html/css'))
 });
 
 gulp.task('js', () => {
   return gulp.src('js/*')
   .pipe(browser.browserify())
   .pipe(strip())
-  .pipe(gulp.dest('docs/js'))
+  .pipe(gulp.dest('public_html/js'))
 });
 
 gulp.task('images', () => {
   return gulp.src('images/*')
   .pipe(imagemin())
-  .pipe(gulp.dest('docs/images/'))
+  .pipe(gulp.dest('public_html/images/'))
 });
 
 gulp.task('fonts', () => {
   return gulp.src('fonts/*')
-  .pipe(gulp.dest('docs/fonts'))
+  .pipe(gulp.dest('public_html/fonts'))
 });
 
 gulp.task('watch', ['default'], () => {
